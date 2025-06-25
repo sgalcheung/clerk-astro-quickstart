44a885157d1d22a050a1d6da46f3dea376976ed6
Sgal Cheung <zhangsigao@live.com>
2025-06-25 18:51:23 +0800

    Refactor code structure for improved readability and maintainability

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
 import clerk from '@clerk/astro'
+
+import netlify from '@astrojs/netlify'
 
 export default defineConfig({
   integrations: [clerk()],
-  adapter: node({ mode: 'standalone' }),
+  adapter: netlify(),
   output: 'server',
 })
